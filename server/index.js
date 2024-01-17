1443acecb94d010982f6fd1fec5ec338ec67ef2b
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2024-01-17 22:18:42 +0530

    Fix Typo

--- server/index.js
+++ server/index.js
@@ -7,16 +7,10 @@ import cookieParser from "cookie-parser";
 import secretRouter from "./routes/secretRoute.js";
 mongoose.set("strictQuery", true);
 
+app.use(cors());
 const app = Express();
 dotenv.config();
 app.use(cookieParser())
-app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-}));
-// app.use(express.urlencoded({extended:true}))
 app.use(Express.json());
 app.use("/user", userRouter);
 app.use("/secret", secretRouter);
