d0dbe8a5fdc97bdfb272b53e9d5d3c43c91cc47f
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2024-01-17 22:26:50 +0530

    Fix Typo

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ import secretRouter from "./routes/secretRoute.js";
 mongoose.set("strictQuery", true);
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
 const app = Express();
 dotenv.config();
 app.use(cookieParser())
