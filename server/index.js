33a237a0ab0ff28ea5a9274cd038df7a699ea6da
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2024-01-18 12:43:17 +0530

    Fix Typo

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 import Express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
+// import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import secretRouter from "./routes/secretRoute.js";
 mongoose.set("strictQuery", true);
 
-app.use(cors());
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://hush-hub.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -17,7 +16,7 @@ app.use(function(req, res, next) {
 })
 const app = Express();
 dotenv.config();
-app.use(cookieParser())
+// app.use(cookieParser())
 app.use(Express.json());
 app.use("/user", userRouter);
 app.use("/secret", secretRouter);
